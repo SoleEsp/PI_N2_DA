82c1212a65d4dc3e525dbf5c18cd314b809fe450
SoleEsp <soledad.espiritu909@gmail.com>
2023-03-01 05:08:41 -0300

    first

--- dashboard.py
+++ dashboard.py
@@ -2,3 +2,20 @@ import streamlit as st
 
 
 st.title('Analisis del Indice S&P 500')
+st.image('https://responsive.fxempire.com/v7/_fxempire_/sites/2/sp500-2.jpg?func=cover&q=70&width=500')
+
+
+st.sidebar.markdown('Evolucion del Indice S&P 500')
+st.markdown('## Introducción')
+
+st.markdown('''
+El índice S&P 500 es uno de los índices más importantes y representativos del mercado de valores estadounidense. Este índice está compuesto por 500 de las empresas más grandes del mercado y es utilizado como una medida de la salud del mercado de valores de EE. UU.
+
+En este informe, realizaremos un análisis del índice S&P 500 utilizando datos históricos desde el año 2000 hasta la actualidad
+''')
+
+st.markdown('## Evolucion Historica')
+
+st.markdown('''
+En general, el índice S&P 500 ha tenido un rendimiento muy sólido desde el año 2000. Si observamos el gráfico de la evolución del índice, podemos ver que hubo un declive significativo durante la crisis financiera de 2008, pero desde entonces el índice ha ido aumentando gradualmente. En particular, el período entre 2012 y 2020 fue un período de crecimiento constante para el índice, con algunas correcciones menores en el camino.
+''')
